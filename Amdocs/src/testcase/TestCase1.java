24f9befe0936b8dd7cdcbd6ce3089273e688d24d
NitheshHS <66508499+NitheshHS@users.noreply.github.com>
2021-08-05 17:17:58 +0530

    testcase1 modified

---
+++ Amdocs/src/testcase/TestCase1.java
@@ -0,0 +1,13 @@
+package testcase;
+/**
+ * 
+ * @author Nitheesha
+ *
+ */
+public class TestCase1 {
+public static void main(String[] args) {
+	
+	System.out.println("testcase 1");
+	System.out.println("testcase 1");
+}
+}
